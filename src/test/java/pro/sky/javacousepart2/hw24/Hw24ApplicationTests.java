e1a42b210562c38d146c51c9081a7df53f675d9f
Pavel-Fomchenkov <donotwritemail@gmail.com>
2023-10-17 22:58:58 +0300

    Initial commit

---
+++ src/test/java/pro/sky/javacousepart2/hw24/Hw24ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.javacousepart2.hw24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
